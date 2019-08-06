6d52a21920275444a848315423766fa196f7483b
Yogaraj Khanal <yrkhanal@gmail.com>
2019-08-06 17:21:07 -0400

    test commit to my new branch

--- src/main/java/io/fabric8/quickstarts/camel/MyTransformer.java
+++ src/main/java/io/fabric8/quickstarts/camel/MyTransformer.java
@@ -32,7 +32,7 @@ public class MyTransformer {
             char letter = (char) ('0' + number);
             buffer.append(letter);
         }
-        return buffer.toString();
+        return "Yogi"+buffer.toString();
     }
 
 }
