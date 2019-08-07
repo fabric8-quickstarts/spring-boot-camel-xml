69cfa568da23a95b5ab0fdbb476506b2382947e1
Yogaraj Khanal <yrkhanal@gmail.com>
2019-08-07 10:36:47 -0400

    refactor code to add just activemq component for testing

--- src/main/java/com/ilink/YogiSpringBoot.java
+++ src/main/java/com/ilink/YogiSpringBoot.java
@@ -23,13 +23,13 @@ import org.springframework.context.annotation.ImportResource;
 @SpringBootApplication
 // load regular Spring XML file from the classpath that contains the Camel XML DSL
 @ImportResource({"classpath:spring/camel-context.xml"})
-public class Application {
+public class YogiSpringBoot {
 
     /**
      * A main method to start this application.
      */
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(YogiSpringBoot.class, args);
     }
 
 }
