d1a90577f705addf4de56ce91745f0e3c7dc8e66
Yogaraj Khanal <yrkhanal@gmail.com>
2019-08-07 15:13:40 -0400

    Added updated AMQ connection

--- src/main/java/com/ilink/YogiSpringBoot.java
+++ src/main/java/com/ilink/YogiSpringBoot.java
@@ -17,12 +17,14 @@
 package com.ilink;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 // load regular Spring XML file from the classpath that contains the Camel XML DSL
 @ImportResource({"classpath:spring/camel-context.xml"})
+@EnableAutoConfiguration
 public class YogiSpringBoot {
 
     /**
---
+++ src/main/java/com/ilink/exception/DataException.java
@@ -0,0 +1,13 @@
+package com.ilink.exception;
+
+public class DataException extends RuntimeException {
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 5100573238766903436L;
+
+	public DataException( String message) {
+        super( message);
+    }
+}
