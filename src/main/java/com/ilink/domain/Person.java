a90569febf92756d5dede89f76e06b3972dacfc8
yogi777 <woodpeckernetwork@gmail.com>
2019-08-09 10:27:23 -0400

    Added Person domain

--- src/main/java/com/ilink/domain/Person.java
+++ src/main/java/com/ilink/domain/Person.java
@@ -14,6 +14,36 @@ public class Person {
 	private String lastName;
 	private String contactNo;
 	private String email;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getContactNo() {
+		return contactNo;
+	}
+	public void setContactNo(String contactNo) {
+		this.contactNo = contactNo;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 	
 
 }
