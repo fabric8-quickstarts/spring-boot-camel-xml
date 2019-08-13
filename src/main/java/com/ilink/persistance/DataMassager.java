8d2e1fe848f3678da93dc15bbd38f60c324e4ebe
yogi777 <woodpeckernetwork@gmail.com>
2019-08-13 08:48:38 -0400

    more refactor

--- src/main/java/com/ilink/persistance/DataMassager.java
+++ src/main/java/com/ilink/persistance/DataMassager.java
@@ -16,9 +16,18 @@ public class DataMassager implements Processor{
 
 	@Override
 	public void process(Exchange exchange) throws Exception {
-		Alien alien= (Alien)	exchange.getIn().getBody()	;
-	
-	 System.out.println("DataMassager--->"+alien);
+		Alien alien= (Alien)	exchange.getOut().getBody()	;
+//	
+//	 System.out.println("DataMassager--->"+alien);
+//	 System.out.println("DataMassager--->"+alien.getAlien_id());
+//	 System.out.println("DataMassager--->"+alien.getFirstName());
+//
+//	 System.out.println("DataMassager--->"+alien.getLastName());
+//	 System.out.println("DataMassager--->"+alien.getAddress());
+//	 System.out.println("DataMassager--->"+alien.getEmail());
+
+	 
+	 
 //	 
 //	 if(ObjectUtils.isEmpty(alien.getAlien_id())){
 //		 throw new DataException("Alien ID is null for " + alien.getFirstName());
