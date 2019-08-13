0c9fec63bb782ff7219603c88cf7f0ee6a0f7b6d
yogi777 <woodpeckernetwork@gmail.com>
2019-08-13 00:41:50 -0400

    equal hashcode and tostring

--- src/main/java/com/ilink/domain/Alien.java
+++ src/main/java/com/ilink/domain/Alien.java
@@ -52,5 +52,63 @@ public class Alien {
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((address == null) ? 0 : address.hashCode());
+		result = prime * result + ((alien_id == null) ? 0 : alien_id.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		result = prime * result + ((transactionType == null) ? 0 : transactionType.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Alien other = (Alien) obj;
+		if (address == null) {
+			if (other.address != null)
+				return false;
+		} else if (!address.equals(other.address))
+			return false;
+		if (alien_id == null) {
+			if (other.alien_id != null)
+				return false;
+		} else if (!alien_id.equals(other.alien_id))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		if (transactionType == null) {
+			if (other.transactionType != null)
+				return false;
+		} else if (!transactionType.equals(other.transactionType))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Alien [transactionType=" + transactionType + ", alien_id=" + alien_id + ", firstName=" + firstName
+				+ ", lastName=" + lastName + ", address=" + address + ", email=" + email + "]";
+	}
 
 }
