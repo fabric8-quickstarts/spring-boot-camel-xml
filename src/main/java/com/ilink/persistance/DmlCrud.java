b9ab778e8114a4b2bb90d6239a584dedd36678b5
yogi777 <woodpeckernetwork@gmail.com>
2019-08-13 00:14:23 -0400

    remvoed more code to make it cleaner

--- src/main/java/com/ilink/persistance/DmlCrud.java
+++ src/main/java/com/ilink/persistance/DmlCrud.java
@@ -17,17 +17,17 @@ public class DmlCrud  implements Processor {
         String tableName ="alien";
         StringBuilder query = new StringBuilder();
 
-        if(alien.getTransactionType().equals("ADD")){
+        //if(alien.getTransactionType().equals("ADD")){
             query.append("INSERT INTO "+tableName+ "(alien_id, first_name,last_name,address,email) VALUES (");
             query.append(alien.getAlien_id()+",'"+alien.getFirstName()+"','"+alien.getLastName()+"','"+alien.getAddress()+"','"+alien.getEmail()+"'"+")");
-
-        }else if(alien.getTransactionType().equals("UPDATE")){
-            query.append("UPDATE "+tableName+" SET email =");
-            query.append(alien.getEmail()+" where alien_id = '"+alien.getAlien_id()+"'");
-
-        }else if(alien.getTransactionType().equals("DELETE")){
-            query.append("DELETE FROM " + tableName + " where alien_id = '"+alien.getAlien_id()+"'");
-        }
+//
+//        }else if(alien.getTransactionType().equals("UPDATE")){
+//            query.append("UPDATE "+tableName+" SET email =");
+//            query.append(alien.getEmail()+" where alien_id = '"+alien.getAlien_id()+"'");
+//
+//        }else if(alien.getTransactionType().equals("DELETE")){
+//            query.append("DELETE FROM " + tableName + " where alien_id = '"+alien.getAlien_id()+"'");
+//        }
         System.out.println("Final Query is : " + query);
         exchange.getIn().setBody(query.toString());
         exchange.getIn().setHeader("alien_id",alien.getAlien_id());		
