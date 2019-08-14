a49301e5d2ed82bf4650adba527c4c52adecfab7
yogi777 <woodpeckernetwork@gmail.com>
2019-08-14 17:10:44 -0400

    Cleanup up some code and put one attachement as proof of concept

--- src/main/java/com/ilink/email/Email.java
+++ src/main/java/com/ilink/email/Email.java
@@ -34,12 +34,12 @@ public class Email implements Processor {
 		  helper.setTo(environment.getProperty("mailto"));
 		  //helper.setSubject("Exception in Camel Route with data attachement encrypted "
 		  //); 
-//		  File file = new File(
-//					getClass().getClassLoader().getResource("application.yml").getFile()
-//				);
-		  helper.setSubject("User Data persited in DB please see attached encrypted data ");
-		  helper.setText("Hello Yogi");
-		 // helper.addAttachment("myattachement-SpringFileAttached",file);
+		  File file = new File(
+					getClass().getClassLoader().getResource("application.yml").getFile()
+				);
+		  helper.setSubject("Congrtas Your visa has been approved !!!!! Have a Safe Journey !!! ");
+		  helper.setText("Data persisted in postgreSQL DB.");
+		  helper.addAttachment("myattachement-SpringFileAttached",file);
 		  emailSender.send((javax.mail.internet.MimeMessage) messageMime);
 		 
 
