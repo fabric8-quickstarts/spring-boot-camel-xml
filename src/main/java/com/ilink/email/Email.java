a9c2841cae5dad9d00d984275eae8e3ae3c8ddaa
yogi777 <woodpeckernetwork@gmail.com>
2019-08-15 12:00:57 -0400

    Updated the messsage for the email

--- src/main/java/com/ilink/email/Email.java
+++ src/main/java/com/ilink/email/Email.java
@@ -37,8 +37,14 @@ public class Email implements Processor {
 		  File file = new File(
 					getClass().getClassLoader().getResource("application.yml").getFile()
 				);
+		  /*
 		  helper.setSubject("Congratulations Your visa has been approved !!!!! Have a Safe Journey !!! ");
 		  helper.setText("Data persisted in postgreSQL DB.");
+		  */
+		  
+		  helper.setSubject("Re: VISA Application");
+		  helper.setText("Dear Concerned: Please find that your VISA application has been approved! The details of the approval will be mailed to your residence in the 7 to 10 business days.");
+		  
 		  helper.addAttachment("myattachement-SpringFileAttached",file);
 		  emailSender.send((javax.mail.internet.MimeMessage) messageMime);
 		 
